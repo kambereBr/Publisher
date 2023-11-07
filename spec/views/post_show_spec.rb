06dbbd6625ea1ce5a96522b36eaa0e2882384e7b
kambereBr <brunokambere@gmail.com>
2023-11-07 23:13:25 +0200

    Fix linters error

--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Post show page', type: :feature do
 
   it 'Display who wrote the post' do
     visit user_post_path(user, post)
-    expect(page).to have_content("#{user.name}")
+    expect(page).to have_content(user.name.to_s)
   end
 
   it 'Display how many comments it has' do
@@ -36,10 +36,10 @@ RSpec.describe 'Post show page', type: :feature do
 
   describe 'Display the username and comment of each commentor' do
     before do
-      user1 = User.create(id: 5, name: 'Bruno',  posts_counter: 1, bio: 'Bio 1', photo: 'https://photos.jpg')
-      user2 = User.create(id: 6, name: 'Obiba',  posts_counter: 1, bio: 'Bio 1', photo: 'https://photos.jpg')
+      user1 = User.create(id: 5, name: 'Bruno', posts_counter: 1, bio: 'Bio 1', photo: 'https://photos.jpg')
+      user2 = User.create(id: 6, name: 'Obiba', posts_counter: 1, bio: 'Bio 1', photo: 'https://photos.jpg')
       post_ = Post.create(title: 'First post', text: 'This is my test', author_id: user1.id, comments_counter: 5,
-                              likes_counter: 0)
+                          likes_counter: 0)
       Comment.create(user_id: user1.id, post_id: post_.id, text: 'Comment 1')
       Comment.create(user_id: user2.id, post_id: post_.id, text: 'Comment 2')
       visit user_post_path(user1, post_)
